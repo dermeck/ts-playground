06d21bf39a7ce61f27b9f3550b75daa5535bd8c1
Martin <dermeck@posteo.de>
2019-10-21 16:46:26 +0200

    mapped types

---
+++ src/2_advanced/2_mapped-types.ts
@@ -0,0 +1,52 @@
+// create types by mapping over another types key/value types
+// [Key in UnionType]: ValueType
+type Person = {
+  name: string;
+  age: number;
+  address?: { location: string; zipCode: number };
+};
+
+type MappedType<T> = {
+  [P in keyof T]: T[P];
+};
+
+type MappedPerson = MappedType<Person>; // same as Person
+
+{
+  {
+    type Promisify<T> = {
+      [P in keyof T]: Promise<T[P]>;
+    };
+
+    type PromisePerson = Promisify<Person>;
+  }
+
+  {
+    // readonly and ? modifier
+    type NonOptionalNonReadonly<T> = {
+      readonly [P in keyof T]?: T[P];
+    };
+    type NonOptionalPerson = NonOptionalNonReadonly<Person>;
+  }
+
+  {
+    type RecursivePartial<T> = {
+      [P in keyof T]?: RecursivePartial<T[P]>;
+    };
+  }
+}
+
+{
+  /* built in mapped types
+
+      Record<Keys, Values> => object with keys of type Keys and values of type Value
+
+      Partial<Object> => all fields optional
+
+      Required<Object> => all fields non-optional
+
+      Readonly<Object> => all fields readonly
+
+      Pick<Object, Keys> => subtype of object with gives Keys 
+   */
+}
